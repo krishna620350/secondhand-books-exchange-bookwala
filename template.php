d2ca84e936311f8e50378d66fec8195690cb72ec
Mukul Mahajan <68242582+19mukul22@users.noreply.github.com>
2021-08-21 10:32:02 +0530

    Add files via upload

---
+++ template.php
@@ -0,0 +1,49 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Template</title>
+
+    <!-- LINKS FOR STYLESHEETS-->
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/bootstrap.css">
+
+</head>
+
+<body>
+
+    <div class="container-fluid" id="outermost-container">
+
+        <!-- Header -->
+        <?php
+        include "include/header.php";
+        ?>
+
+
+        <!--------------- TEMPLATE-------------->
+        <!-------- THIS IS BLANK SPACE, WRITE YOUR CODE HERE ----------->
+        <!----------------------------------------->
+
+
+
+        <!-- Including Footer-->
+        <?php
+        include "include/footer.php";
+        ?>
+
+        
+
+
+    </div> <!-- END OF OUTERMOST CONTAINER-->
+
+</body>
+
+<script src="js/jquery-3.6.0.js"></script>
+<script>
+</script>
+
+</html>
