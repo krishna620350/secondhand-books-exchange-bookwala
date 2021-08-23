50acd3d8acee9bae03c5a5a417eabc01ddf0904e
muskan05tulsiyani <muskan.tulsiyani580@gmail.com>
2021-08-23 19:21:07 +0530

    this is about us

--- template.php
+++ template.php
@@ -19,21 +19,37 @@
     <div class="container-fluid" id="outermost-container">
 
         <!-- Header -->
-        <?php
-        include "include/header.php";
-        ?>
+        <!--  <?php
+        //include "include/header.php"; -->
+        ?>  -->
 
 
         <!--------------- TEMPLATE-------------->
         <!-------- THIS IS BLANK SPACE, WRITE YOUR CODE HERE ----------->
         <!----------------------------------------->
+        <div class="header-1">
+            <div class="main-1">
+                <h1>About Us</h1>
+                <p>
+                    Our main objective is “Unstoppable Learning” as to create a workspace where a user can sell or buy his second hand books
+                    which are of no use to him as he has passed the session & entered a new Phase & his requirements of new books will be
+                    a challenge for him but those second hand books he has used can help other people as well as knowledge & learning only 
+                    depends on Oneself.  
+                    On Our Website He will be able to buy & sell his books according to his requirements. 
+                   
+                     As we provide a user friendly platform where people can exchange books by buying or selling them as per there 
+                    personal needs and ease there living.
+                </p>
+            </div>
+        </div>
+        
 
 
 
         <!-- Including Footer-->
-        <?php
-        include "include/footer.php";
-        ?>
+        <!-- <?php
+        // include "include/footer.php";
+        ?> -->
 
         
 
